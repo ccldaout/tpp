7479abed558152181f5e370912c56329ac7195aa
Masakazu Iwanaga <ccldaout@gmail.com>
2016-12-27 11:58:25 +0900

    pycode: added

---
+++ pycode.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import dis
+import marshal
+import types
+
+def dis_code(c, upper):
+    upper = upper + '.' + c.co_name
+    for c2 in c.co_consts:
+        if isinstance(c2, types.CodeType):
+            dis_code(c2, upper)
+            print
+    print '[ co_name: %s ] --------' % upper, c
+    print '  co_varnames :', c.co_varnames
+    print '  co_names    :', c.co_names
+    print '  co_freevars :', c.co_freevars
+    print '  co_cellvars :', c.co_cellvars
+    print '  co_flags    : 0o%o' % c.co_flags 
+    dis.dis(c)
+
+def dis_pyc(pyc):
+     with open(pyc, 'rb') as f:
+         dis_code(marshal.loads(f.read()[8:]), '')
+
+def dis_py(py):
+    with open(py) as f:
+        dis_code(compile(f.read(), py, 'exec'), '')
+
+if __name__ == '__main__':
+    import sys
+    f = sys.argv[1]
+    if f.endswith('.py'):
+        dis_py(f)
+    elif f.endswith(('.pyc', '.pyo')):
+        dis_pyc(f)
