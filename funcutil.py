5f046582188d1881f3a7a49a2ba02cf867ce52a8
Masakazu Iwanaga <ccldaout@gmail.com>
2017-01-13 11:26:20 +0900

    funcutil: bug fix

--- funcutil.py
+++ funcutil.py
@@ -22,8 +22,12 @@ def get_sig(aspec):
     if aspec.keywords:
         yield '**' + aspec.keywords
 
-def get_kws(aspec):
-    for a in aspec.args:
+def get_kws(aspec, rename_self=False):
+    args=list(aspec.args)
+    if rename_self and args and args[0] == 'self':
+        yield 'self_original=%s' % args[0]
+        args.pop(0)
+    for a in args:
         yield '%s=%s' % (a, a)
     if aspec.varargs:
         yield '*' + aspec.varargs
@@ -41,9 +45,9 @@ def get_vns(aspec):
 def prehook_wrapper(f, prehook):
     aspec = inspect.getargspec(f)
 
-    f_name = f.__name__
     f_sig = ', '.join(get_sig(aspec))    
     f_kws = ', '.join(get_kws(aspec))     
+    f_kws2 = ', '.join(get_kws(aspec, rename_self=True))
 
     vns = set(get_vns(aspec))
     pre_name = 'prehook'
@@ -51,6 +55,12 @@ def prehook_wrapper(f, prehook):
         if pre_name not in vns:
             break
         pre_name += '_'
+
+    f_name = f.__name__
+    for _ in xrange(len(''.join(vns))):
+        if f_name not in vns:
+            break
+        f_name += '_'
 
     m_name = 'maker'
     src = '''def %s(%s, %s):
@@ -60,7 +70,7 @@ def prehook_wrapper(f, prehook):
     return _%s''' % (
     m_name, f_name, pre_name,
     f_name, f_sig,
-    pre_name, f_kws,
+    pre_name, f_kws2,
     f_name, f_kws,
     f_name)
 
